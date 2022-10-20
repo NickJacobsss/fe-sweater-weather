e5a8acfac31f998f7a0484c88657d3ef4d277263
Nick Jacobs <nickjacobsatc@gmail.com>
2022-10-20 15:36:31 -0500

    Feat: create_user added

---
+++ app/services/user_service.rb
@@ -0,0 +1,15 @@
+class UserService
+
+  def self.connection
+    Faraday.new(url: 'https://be-sweater-weather.herokuapp.com/api/v1/')
+  end
+
+  def self.create_user(user_data)
+      response = connection.post('users') do |user|
+        user.params[:email] = user_data[:email]
+        user.params[:password] = user_data[:password]
+        user.params[:password_confirmation] = user_data[:password_confirmation]
+      end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
